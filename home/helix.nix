3e563eff0f17bbbd27b41fe718bfcd549c181018
DaraJKong <daraetkong@gmail.com>
2024-03-04 03:42:08 -0500

    Helix mocha config + Nix LSP & fmt

--- home/helix.nix
+++ home/helix.nix
@@ -1,7 +1,85 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.helix = {
     enable = true;
+    package = inputs.helix.packages.${pkgs.system}.default;
+    settings = {
+      theme = "catppuccin_mocha";
+      keys.normal = {
+        "{" = "goto_prev_paragraph";
+        "}" = "goto_next_paragraph";
+        "X" = "extend_line_above";
+        "esc" = ["collapse_selection" "keep_primary_selection"];
+        C-j = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+        C-k = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
+      };
+      keys.insert = {
+        C-c = ["normal_mode" "collapse_selection" "keep_primary_selection"];
+      };
+      editor = {
+        true-color = true;
+        mouse = false;
+        idle-timeout = 1;
+        bufferline = "always";
+        line-number = "relative";
+        color-modes = true;
+        cursorline = true;
+        scrolloff = 6;
+        soft-wrap.enable = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "block";
+        };
+        indent-guides = {
+          render = true;
+          character = "▏";
+        };
+        lsp = {
+          display-messages = true;
+          display-inlay-hints = true;
+        };
+        gutters = ["diagnostics" "line-numbers" "spacer" "diff"];
+        statusline = {
+          left = ["mode" "selections" "spinner" "file-name" "total-line-numbers"];
+          center = [];
+          right = ["diagnostics" "file-encoding" "file-line-ending" "file-type" "position-percentage" "position"];
+          mode = {
+            normal = "NOR";
+            insert = "INS";
+            select = "SEL";
+          };
+        };
+      };
+    };
+    languages = {
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+        }
+      ];
+      language-server = {
+        nil = {
+          command = lib.getExe pkgs.nil;
+          config.nil.formatting.command = ["${lib.getExe pkgs.alejandra}" "-q"];
+        };
+      };
+    };
   };
+
+  home.packages = with pkgs; [
+    xclip
+    nil
+    alejandra
+    rust-analyzer
+    cargo
+  ];
 
   home.sessionVariables = {
     EDITOR = "helix";
