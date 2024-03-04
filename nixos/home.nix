f56312250b207ac01c5295580b2763927e6aeca9
DaraJKong <daraetkong@gmail.com>
2024-03-03 23:11:56 -0500

    Fix GNOME's keyboard layout with home-manager

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -14,6 +14,15 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # Set GNOME's keyboard layout
+  dconf.settings = {
+      "org/gnome/desktop/input-sources" = {
+        show-all-sources = true;
+        sources = [ (lib.hm.gvariant.mkTuple [ "xkb" "ca" ]) ];
+        xkb-options = [ ];
+      };
+  };
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
