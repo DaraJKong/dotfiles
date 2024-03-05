8fa883abbbf0468efe4e1b14151e663b3c400fcc
DaraJKong <daraetkong@gmail.com>
2024-03-05 02:51:23 -0500

    Add ripgrep

--- home/default.nix
+++ home/default.nix
@@ -29,7 +29,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -46,6 +46,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    ripgrep
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
