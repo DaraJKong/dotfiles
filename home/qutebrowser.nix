237eda61619e21b082b1c85d72033dcdb3c14e38
DaraJKong <daraetkong@gmail.com>
2024-03-05 23:01:05 -0500

    Add Qutebrowser quickmarks

--- home/qutebrowser.nix
+++ home/qutebrowser.nix
@@ -9,8 +9,24 @@
     settings = {
       colors.webpage = {
         darkmode.enabled = true;
+        darkmode.policy.images = "never";
         preferred_color_scheme = "dark";
       };
+    };
+    quickmarks = {
+      # Nix related
+      packages = "https://search.nixos.org/packages";
+      options = "https://search.nixos.org/options";
+      mynixos = "https://mynixos.com/";
+
+      nixpkgs = "https://github.com/NixOS/nixpkgs";
+      home-manager = "https://github.com/nix-community/home-manager";
+
+      # Programming
+      github = "https://github.com/";
+
+      # AI tools
+      phind = "https://www.phind.com/search?home=true";
     };
   };
 }
