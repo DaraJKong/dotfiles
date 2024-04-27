030ceb5e574b9c846c918110022daf128adff357
DaraJKong <daraetkong@gmail.com>
2024-04-27 15:06:31 -0400

    Integrate Zellij with Nushell

--- home/cli/nushell.nix
+++ home/cli/nushell.nix
@@ -1,18 +1,39 @@
 {...}: {
   programs.nushell = {
     enable = true;
-    extraConfig = ''
-      # Nushell configuration here
+    envFile.text = ''
       $env.STARSHIP_SHELL = "nu"
+
+      $env.ZELLIJ_AUTO_ATTACH = "false"
+      $env.ZELLIJ_AUTO_EXIT = "false"
+    '';
+    configFile.text = ''
       def create_left_prompt [] {
         starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
       }
+
       $env.PROMPT_COMMAND = { || create_left_prompt }
       $env.PROMPT_COMMAND_RIGHT = ""
       $env.PROMPT_INDICATOR = ""
       $env.PROMPT_INDICATOR_VI_INSERT = ": "
       $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
       $env.PROMPT_MULTILINE_INDICATOR = "::: "
+
+      def start_zellij [] {
+        if 'ZELLIJ' not-in ($env | columns) {
+          if 'ZELLIJ_AUTO_ATTACH' in ($env | columns) and $env.ZELLIJ_AUTO_ATTACH == 'true' {
+            zellij attach -c
+          } else {
+            zellij
+          }
+
+          if 'ZELLIJ_AUTO_EXIT' in ($env | columns) and $env.ZELLIJ_AUTO_EXIT == 'true' {
+            exit
+          }
+        }
+      }
+
+      start_zellij
     '';
   };
 }
