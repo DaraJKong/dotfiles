2f5e9ce22c8d4845ef700f5f777f7f7a6e1d1dd7
DaraJKong <daraetkong@gmail.com>
2024-05-13 16:19:53 -0400

    Remove nu's welcome message

--- home/cli/nushell.nix
+++ home/cli/nushell.nix
@@ -12,13 +12,6 @@
         starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
       }
 
-      $env.PROMPT_COMMAND = { || create_left_prompt }
-      $env.PROMPT_COMMAND_RIGHT = ""
-      $env.PROMPT_INDICATOR = ""
-      $env.PROMPT_INDICATOR_VI_INSERT = ": "
-      $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
-      $env.PROMPT_MULTILINE_INDICATOR = "::: "
-
       def start_zellij [] {
         if 'ZELLIJ' not-in ($env | columns) {
           if 'ZELLIJ_AUTO_ATTACH' in ($env | columns) and $env.ZELLIJ_AUTO_ATTACH == 'true' {
@@ -32,6 +25,15 @@
           }
         }
       }
+
+      $env.config.show_banner = false
+
+      $env.PROMPT_COMMAND = { || create_left_prompt }
+      $env.PROMPT_COMMAND_RIGHT = ""
+      $env.PROMPT_INDICATOR = ""
+      $env.PROMPT_INDICATOR_VI_INSERT = ": "
+      $env.PROMPT_INDICATOR_VI_NORMAL = "〉"
+      $env.PROMPT_MULTILINE_INDICATOR = "::: "
 
       start_zellij
     '';
