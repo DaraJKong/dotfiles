a88a4582e84906e80c0420c9422540ed8d5ea14e
DaraJKong <daraetkong@gmail.com>
2024-05-22 15:00:19 -0400

    Add devshell (unused) & flake update

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # devshell = {
+    #   url = "github:numtide/devshell";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -31,12 +35,45 @@
     ...
   } @ inputs: let
     system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
   in {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs system;};
         modules = [
           ./system/configuration.nix
+        ];
+      };
+    };
+
+    devShells.${system} = {
+      "rust-stable" = {
+        name = "rust-stable";
+        commands = {};
+        env = {};
+        packages = with pkgs; [
+          alejandra
+          # cargo
+          cargo-leptos
+          clang
+          deadnix
+          leptosfmt
+          nil
+          openssl
+          pkg-config
+          # rust-analyzer
+          (rust-bin.stable.latest.default.override {
+            extensions = ["rust-analyzer" "rust-src" "rust-std" "rustfmt" "clippy"];
+            targets = ["x86_64-unknown-linux-gnu" "wasm32-unknown-unknown"];
+          })
+          sqlite
+          sqlx-cli
+          statix
+          tailwindcss
+          tailwindcss-language-server
+          taplo
         ];
       };
     };
