294c3dbb00fb7b1481766de8cc28129bfd13501f
DaraJKong <daraetkong@gmail.com>
2024-07-06 00:31:12 -0400

    Power management and battery life

--- hosts/mayushii/default.nix
+++ hosts/mayushii/default.nix
@@ -5,6 +5,44 @@
 
   # TODO: Add laptop power saving and battery life optimisations
 
+  # Better scheduling for CPU cycles
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+
+  # Enable TLP
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+    };
+  };
+
+  # Disable GNOME's power management
+  services.power-profiles-daemon.enable = false;
+
+  # Enable powertop
+  powerManagement.powertop.enable = true;
+
+  # Enable thermald (only necessary if on Intel CPUs)
+  services.thermald.enable = true;
+
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=no
+  '';
+
+  # services.udev.extraRules = ''
+  #   ACTION=="add", SUBSYSTEM=="usb", DRIVER=="usb", ATTR{power/wakeup}="enabled"
+  # '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
